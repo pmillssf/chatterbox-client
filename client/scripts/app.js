adc507e42f2ce92109cd7fb71709f830bf02c8c6
Hack Reactor Students <accounts@hackreactor.com>
2017-02-17 15:34:04 -0800

    Create app behavior methods

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,26 @@
 // YOUR CODE HERE:
+var app = {
+  dataPulled: undefined,
+  init: function() {},
+  send: function() {},
+  fetch: function() {},
+  clearMessages: function() {},
+  renderMessage: function() {},
+  renderRoom: function() {},
+};
+$.ajax({
+  // This is the url you should use to communicate with the parse API server.
+  url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+  type: 'GET',
+  contentType: 'application/json',
+  success: function (data) {
+    app.dataPulled = data;
+    console.log('chatterbox: Message sent');
+    console.log(data);
+    return data;
+  },
+  error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message', data);
+  }
+});
